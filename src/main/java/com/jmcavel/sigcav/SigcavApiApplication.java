9a1669ec3f4161bb9ff1107bdd3916061d9579a9
Wilian Lopez <wlopezvilcarromero.hotmail.com>
2026-06-10 16:18:08 -0500

    Initial commit - Spring Boot API

---
+++ src/main/java/com/jmcavel/sigcav/SigcavApiApplication.java
@@ -0,0 +1,13 @@
+package com.jmcavel.sigcav;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SigcavApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SigcavApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jmcavel/sigcav/SigcavApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jmcavel.sigcav;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SigcavApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
