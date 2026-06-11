789d90933647bac762cfea341a6fcad97480d8d0
Dayanna-Calderon19 <aracellycalderonlozano28@gmail.com>
2026-06-11 10:16:04 -0500

    feat: Agregando excepciones personalizadas en el módulo del Cliente

---
+++ src/main/java/com/jmcavel/sigcav/exception/ReglaNegocioException.java
@@ -0,0 +1,8 @@
+package com.jmcavel.sigcav.exception;
+
+public class ReglaNegocioException extends RuntimeException {
+
+    public ReglaNegocioException(String mensaje) {
+        super(mensaje);
+    }
+}
