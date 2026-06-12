001dc3625ee864b49c0c4fbdb89acea5a637669d
Wilian Lopez <wlopezvilcarromero.hotmail.com>
2026-06-12 14:55:59 -0500

    fix: corregir servicio de auditoría y agregar valores faltantes a enums

--- src/main/java/com/jmcavel/sigcav/enums/AccionAuditoria.java
+++ src/main/java/com/jmcavel/sigcav/enums/AccionAuditoria.java
@@ -3,8 +3,11 @@ package com.jmcavel.sigcav.enums;
 public enum AccionAuditoria {
     CREAR,
     EDITAR,
+    ACTUALIZAR,
     ELIMINAR,
     ANULAR,
+    ACTIVAR,
+    DESACTIVAR,
     LOGIN,
     LOGOUT,
     CAMBIO_ESTADO,
